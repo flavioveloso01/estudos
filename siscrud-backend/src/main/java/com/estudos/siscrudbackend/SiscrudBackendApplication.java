6d51ca928e328b4dbecf5b73ffd73b025b84ff28
x03227283123 <x03227283123@tcu.gov.br>
2023-03-07 15:33:25 -0300

    adicionando estrutura inicial de projeto backend

---
+++ siscrud-backend/src/main/java/com/estudos/siscrudbackend/SiscrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.siscrudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscrudBackendApplication.class, args);
+	}
+
+}
---
+++ siscrud-backend/src/test/java/com/estudos/siscrudbackend/SiscrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.siscrudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
